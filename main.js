a5cb3028037773f61a3579037fe5f9ab31d3cbc6
Shalomraz <shalomraz76@gmail.com>
2015-07-23 19:53:05 +0300

    Display images file names

---
+++ main.js
@@ -0,0 +1,65 @@
+(function ($window, $document) {
+
+	var utils = {
+		q: function (selector) {
+			return $document.querySelector(selector);
+		},
+		prepend: function (parent, child) {
+			parent.insertBefore(child, parent.firstChild)
+		},
+		create: function (element) {
+			return $document.createElement(element);
+		}
+	};
+
+
+	$window.utils = utils;
+
+
+}(window, window.document));
+
+
+
+(function ($window, $document, $) {
+
+	var file = $.q("#capture");
+	var list = $.q(".photos");
+
+	file.addEventListener("change", fileChosen);
+
+	function fileChosen (e) {
+		var file = e.target.files[0];
+		addToList(file);
+	}
+
+	function addToList (file) {
+		$.prepend(list, wrapWithTemplate(file));
+	}
+
+	function wrapWithTemplate (file) {
+		var img = wrapWithImg(file);
+		var link = wrapWithLink(img, file);
+		var li = wrapWithLi(link, file);
+		return li;
+	}
+
+	function wrapWithImg (file) {
+		var img = $.create("img");
+		img.alt = file.name;
+		return img;
+	}
+
+	function wrapWithLink (element, file) {
+		var link = $.create("a");
+		link.appendChild(element);
+		return link;
+	}
+
+	function wrapWithLi (element, file) {
+		var li = $.create("li");
+		li.appendChild(element);
+		return li;
+	}
+
+
+}(window, window.document, window.utils));
